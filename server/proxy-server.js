68590711b1309e3bef4b9e68c04ef78648ee38a3
tmkuba <tmk.rss@gmail.com>
2018-03-24 11:56:36 -0700

    Refactor proxy server some

--- server/proxy-server.js
+++ server/proxy-server.js
@@ -29,22 +29,30 @@ const URLs = {
 const statistics = {
   cacheHit: 0,
   cacheMiss: 0,
-  axiosCalls: 0,
+  lookupCount: {},
 };
-
-const lookupCount = {};
 
 const html = fs.readFileSync(path.join(__dirname, 'dist', 'ssr-template.html'), 'utf8');
 const ssrTemplate = _.template(html);
 
+const sendResponse = (res, code, mimeType, data) => {
+  if (mimeType === '') {
+    res.writeHead(code);
+  } else {
+    res.writeHead(code, {
+      'Content-Type': mimeType,
+    });
+  }
+  res.end(data);
+};
+
 const server = http.createServer((req, res) => {
   if (req.method !== 'GET') {
-    res.writeHead(404);
-    res.end();
+    sendResponse(res, 404, '', '404 Not Found');
   } else if (req.url.startsWith('/restaurants/')) {
     // handle ajax call for restaurant reviews
     const [, , id, component] = req.url.split('/');
-    lookupCount[id] = lookupCount[id] ? lookupCount[id] + 1 : 1;
+    statistics.lookupCount[id] = statistics.lookupCount[id] ? statistics.lookupCount[id] + 1 : 1;
     // console.log(`GET ${component} to ${URLs.reviews}/restaurants/${id}/reviews`);
     const redisKey = `reviews:${id}`;
     redisClient.getAsync(redisKey)
@@ -52,26 +60,19 @@ const server = http.createServer((req, res) => {
         if (redisVal === null) {
           axios.get(`${URLs.reviews}/restaurants/${id}/reviews`)
             .then((response) => {
-              res.writeHead(response.status, {
-                'Content-Type': 'application/json',
-              });
               const jsonString = JSON.stringify(response.data);
-              res.end(jsonString);
+              sendResponse(res, 200, 'application/json', jsonString);
               redisClient.set(redisKey, jsonString, 'EX', REDIS_LIFETIME); // cache for REDIS_LIFETIME secs
             })
             .catch((error) => {
               console.log(error, 'Error');
-              res.statusCode = error.response ? error.response.status : 500;
+              const errCode = error.response ? error.response.status : 500;
               const errMsg = error.response ? error.response.statusText : 'Internal Server Error';
-              res.end(errMsg);
+              sendResponse(res, errCode, '', errMsg);
             });
           statistics.cacheMiss += 1;
-          statistics.axiosCalls += 1;
         } else {
-          res.writeHead(200, {
-            'Content-Type': 'application/json',
-          });
-          res.end(redisVal);
+          sendResponse(res, 200, 'application/json', redisVal);
           statistics.cacheHit += 1;
         }
       });
@@ -87,17 +88,11 @@ const server = http.createServer((req, res) => {
           const markup = ReactDom.renderToString(component);
 
           const result = ssrTemplate({ id, markup });
-          res.writeHead(200, {
-            'Content-Type': 'text/html',
-          });
-          res.end(result);
+          sendResponse(res, 200, 'text/html', result);
           redisClient.set(redisKey, result, 'EX', REDIS_LIFETIME); // cache for REDIS_LIFETIME secs
           statistics.cacheMiss += 1;
         } else {
-          res.writeHead(200, {
-            'Content-Type': 'text/html',
-          });
-          res.end(redisVal);
+          sendResponse(res, 200, 'text/html', redisVal);
           statistics.cacheHit += 1;
         }
       });
@@ -113,28 +108,18 @@ const server = http.createServer((req, res) => {
           fs.readFile(filename, (err, data) => {
             if (err) {
               console.log(err);
-              res.statusCode = 404;
-              res.end('404 Not Found');
+              sendResponse(res, 404, '', '404 Not Found');
             } else {
-              res.writeHead(200, {
-                'Content-Type': mime.lookup(filename),
-              });
-              res.end(data);
-
+              sendResponse(res, 200, mime.lookup(filename), data);
               const encoding = mime.lookup(filename) === 'image/jpeg' ? 'base64' : 'utf8';
               const bufferString = data.toString(encoding);
-              // console.log(`${redisKey} setting to ${bufferString.slice(0, 20)}`);
               redisClient.set(redisKey, bufferString, 'EX', REDIS_LIFETIME); // cache for REDIS_LIFETIME secs
             }
             statistics.cacheMiss += 1;
           });
         } else {
-          // console.log(`${redisKey} resolves to ${redisVal.slice(0, 20)}`);
-          res.writeHead(200, {
-            'Content-Type': mime.lookup(filename),
-          });
           const returnData = mime.lookup(filename) === 'image/jpeg' ? new Buffer(redisVal, 'base64') : redisVal;
-          res.end(returnData);
+          sendResponse(res, 200, mime.lookup(filename), returnData);
           statistics.cacheHit += 1;
         }
       });
@@ -142,7 +127,7 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-  console.log('NewRelic', newrelic.agent.config.license_key.slice(0, 10),'...');
+  console.log('NewRelic', newrelic.agent.config.license_key.slice(0, 10), '...');
   console.log(`listening on port ${port}`);
 });
 
@@ -151,11 +136,10 @@ process.on('SIGINT', () => {
   console.log('\nRedis hits %', (100 * (statistics.cacheHit / total)).toFixed(1));
   console.log('Redis misses %', (100 * (statistics.cacheMiss / total)).toFixed(1));
   console.log('Total lookups', total);
-  console.log('Axios calls', statistics.axiosCalls);
 
-  const multiLookups = Object.values(lookupCount).filter(count => count > 1);
-  const singleLookups = Object.values(lookupCount).filter(count => count === 1);
-  console.log(`multi: ${multiLookups.length}, single: ${singleLookups.length}`);
+  const multiLookups = Object.values(statistics.lookupCount).filter(count => count > 1);
+  const singleLookups = Object.values(statistics.lookupCount).filter(count => count === 1);
+  console.log(`multi-lookups: ${multiLookups.length}, single-lookups: ${singleLookups.length}`);
 
   process.exit();
 });
